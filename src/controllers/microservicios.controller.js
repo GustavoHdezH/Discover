d91b8dbbfa8ec9cce0c4a1731eda6137f9abdaf2
ghernandez <ghernandez@cxc.com.mx>
2021-07-21 23:14:39 -0500

    Se agrega funcion para crear obejto y se renderiza en vista

--- src/controllers/microservicios.controller.js
+++ src/controllers/microservicios.controller.js
@@ -1,17 +1,33 @@
 const controllermicroservicios = {};
 
+const Servicios = require('../models/servicios');
+
 // Renderizar el formulario
 controllermicroservicios.renderMicroserviciosForm = (req, res) =>{
     //res.send('microservicio agregado');
     res.render('microservicios/new-microservicio');
 };
 //Crear microservicio
-controllermicroservicios.createMicroservicios = (req, res) =>{
-    console.log(req.body)
+controllermicroservicios.createMicroservicios = async (req, res) =>{
+    //Se obtienen los datos desde el formulario
+    const {container, port, server, discover, code, team} = req.body;
+    //Se pasan las propiedades que se requiere para crear el objeto
+    const newServicios = Servicios({
+        container,port, server, discover, code, team
+    })
+    //Se guarda el objeto en la colleccion
+    await newServicios.save();
+    console.log(newServicios)
     res.send('Nuevo microservicio');
 };
-controllermicroservicios.renderMicroservicios = (req, res) =>{
-    res.send('render microservicios');
+// Renderizar microservicios
+controllermicroservicios.renderMicroservicios = async (req, res) =>{
+    //find({}).lean(); resuelve el problema
+    //Handlebars: Access has been denied to resolve the property
+    const servicios = await Servicios.find({}).lean();
+    // Se renderiza la vista en el navegador y se pasan los microservicios como objeto
+    res.render('microservicios/all-microservicios', { servicios });
+    
 };
 
 // Editar microservicios
